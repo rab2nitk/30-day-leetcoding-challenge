2fddc093a76fccc2808ef76052d04fe6dcc911d9
rab2nitk@gmail.com <rab2nitk@gmail.com>
2020-04-12 16:01:47 +0530

    removed unnecessary code

--- 30 Days LeetCode Challenge/src/week1/MoveZeros.java
+++ 30 Days LeetCode Challenge/src/week1/MoveZeros.java
@@ -2,17 +2,6 @@ package src.week1;
 class Solution {
     public void moveZeroes(int[] nums) {
         int count = 0;
-        /*for(int i=0;i<nums.length;i++){
-           
-            if(nums[i]>0){
-                count++;
-                System.out.print(nums[i]);
-                nums[count] = nums[i];
-            }
-            
-            
-        }*/
-        
         for (int i = 0; i < nums.length; i++) 
             if (nums[i] != 0) 
                 nums[count++] = nums[i];
